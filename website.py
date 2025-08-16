46535ef7fe6becf6eff11e9ab7b6da2d94eacef3
ryan95young <rgyoung1995@gmail.com>
2025-08-15 20:56:29 -0400

    Update website.py

--- website.py
+++ website.py
@@ -186,10 +186,10 @@ if st.button("Submit"):
     if comment.strip():
         if "submissions" not in st.session_state:
             st.session_state.submissions = []
-        st.session_state.submissions.append({"comment": comment})
+            st.session_state.submissions.append({"comment": comment})
 
             if submission_final_reply == "Commentary"
-            save_commentary(comment)
+                save_commentary(comment)
         
         st.success(submission_final_reply)
     
