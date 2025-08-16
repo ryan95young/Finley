e05a8ab286733267090ee07f3a0f620dfe2357bc
ryan95young <rgyoung1995@gmail.com>
2025-08-15 22:35:14 -0400

    Update commentary_upload_supabase.py

--- input_text/commentary_upload_supabase.py
+++ input_text/commentary_upload_supabase.py
@@ -1,5 +1,5 @@
 import json
-from datetime import datetime, timezone
+from datetime import datetime
 from input_text.commentary_sanitize_text import sanitize_text
 from supabase import create_client
 import uuid
@@ -12,17 +12,14 @@ supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def save_commentary(comment):
 
-    try:
-        utc_timestamp = datetime.now(timezone.utc).isoformat()
-    except Exception:
-        utc_timestamp = datetime.utcnow().isoformat() + "Z"
+    utc_timestamp = datetime.utcnow().isoformat() + "Z"
 
     # Save to Supabase
     new_id = str(uuid.uuid4())
     comment_json = {"text": comment}
     response = supabase.table("Finley_Testing").insert({
-    "id": new_id,
-    "user_id": "ryan-young",
-    "timestamp": utc_timestamp,
-    "raw_json": comment_json
+        "id": new_id,
+        "user_id": "ryan-young",
+        "timestamp": utc_timestamp,
+        "raw_json": comment_json
 }).execute()
