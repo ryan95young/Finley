5978e4fc730ab70c37849a4f42cfa1029ace57a8
ryan95young <rgyoung1995@gmail.com>
2025-08-15 23:29:37 -0400

    Update commentary_upload_supabase.py

--- input_text/commentary_upload_supabase.py
+++ input_text/commentary_upload_supabase.py
@@ -4,26 +4,13 @@ from input_text.commentary_sanitize_text import sanitize_text
 from supabase import create_client
 import uuid
 import os
-from openai import OpenAI
 
 # Supabase client
 SUPABASE_URL = "https://wvhydcduidfgmswawkud.supabase.co"
 SUPABASE_KEY = "sb_publishable_xzumVoItFr43myzD6Qle_w_fyI8W6Q6"
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-OPENAI_API_KEY = "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"
-
-#OpenAI client
-openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
 def save_commentary(comment):
-
-    embedding_response = openai_client.embeddings.create(
-        model="text-embedding-3-small",
-        input=comment
-    )
-
-    embedding = embedding_response.data[0].embedding
 
     # Save to Supabase
     new_id = str(uuid.uuid4())
@@ -34,5 +21,4 @@ def save_commentary(comment):
         "user_id": "ryan-young",
         "timestamp": utc_timestamp,
         "raw_json": comment_json, 
-        "embedding": embedding
 }).execute()
